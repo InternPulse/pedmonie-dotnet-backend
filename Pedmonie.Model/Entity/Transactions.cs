14aa3196f1492a7ad97505f1bc7847386fcaa048
Adnanahmad <adnanhashim1212@gmail.com>
2025-02-23 21:37:41 +0100

    Update Transactions.cs

--- Pedmonie.Model/Entity/Transactions.cs
+++ Pedmonie.Model/Entity/Transactions.cs
@@ -81,9 +81,9 @@ public class Transaction
 
     // Unique transaction ID, which is a UUID represented as a string (char(36))
     [Required]
-    [Column("transaction_id")]
+    [Column("transaction_id", TypeName = "varchar(32)")]
     [StringLength(32)]
-    public Guid TransactionId { get; set; }
+    public string TransactionId { get; set; }
 
     // Order ID represented as a UUID Guid (char(36))
     [Required]
