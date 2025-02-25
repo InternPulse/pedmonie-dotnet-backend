7f9c74c9a9b74272b96ba3c8e4d63d5cf1910520
Adnanahmad <adnanhashim1212@gmail.com>
2025-02-25 15:03:24 +0100

    Update IWalletService.cs

--- Pedmonie.Service/Interfaces/IWalletService.cs
+++ Pedmonie.Service/Interfaces/IWalletService.cs
@@ -8,5 +8,5 @@ using Pedmonie.Model.Entity;
 namespace Pedmonie.Service.Interfaces;
 public interface IWalletService
 {
-    Task<BaseResponse<Wallet>> GetWalletByIdAsync(Guid walletId);
+    Task<BaseResponse<Wallet>> GetWalletByIdAsync(string walletId);
 }
